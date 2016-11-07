1cba73de51847d83319cc4b4e02fe9fa26d59a46
garyhuang <834865081@qq.com>
2016-11-07 09:37:09 +0800

    commit config

---
+++ config/创建表.sql
@@ -0,0 +1,91 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.17-MariaDB : Database - dragon
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `sys_grepu_per`;
+
+CREATE TABLE `sys_grepu_per` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `groupId` int(11) DEFAULT '0' COMMENT '组ID',
+  `permissionId` int(11) DEFAULT NULL COMMENT '权限id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_grepu_per` */
+
+/*Table structure for table `sys_group` */
+
+DROP TABLE IF EXISTS `sys_group`;
+
+CREATE TABLE `sys_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `gname` varchar(80) DEFAULT '' COMMENT '权限组名称',
+  `remark` varchar(150) DEFAULT '' COMMENT '描述',
+  `createdate` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_group` */
+
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `menuname` varchar(80) DEFAULT '' COMMENT '菜单名称',
+  `icon` varchar(30) DEFAULT 'icon-nav' COMMENT '菜单图标',
+  `url` varchar(120) DEFAULT '' COMMENT 'url',
+  `pid` int(11) DEFAULT '0' COMMENT '父ID',
+  `createDate` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_permission` */
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `loginName` varchar(30) DEFAULT '' COMMENT '登录名称',
+  `pwd` varchar(64) DEFAULT '' COMMENT '密码',
+  `phone` varchar(20) DEFAULT '' COMMENT '手机号码',
+  `email` varchar(80) DEFAULT '' COMMENT '邮箱账号',
+  `createDate` datetime DEFAULT NULL COMMENT '系统时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`loginName`,`pwd`,`phone`,`email`,`createDate`) values (1,'admin','123456','12','122','2016-09-20 14:24:23');
+
+/*Table structure for table `sys_user_group` */
+
+DROP TABLE IF EXISTS `sys_user_group`;
+
+CREATE TABLE `sys_user_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `userid` int(11) DEFAULT NULL COMMENT '管理员id',
+  `groupid` int(11) DEFAULT NULL COMMENT '组id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user_group` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
